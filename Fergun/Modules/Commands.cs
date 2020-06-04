ebc7fd208e1b0d31bd974d43c395de7cf4f12c9c
d4n3436 <39978852+d4n3436@users.noreply.github.com>
2020-06-04 14:01:05 -0500

    Add files via upload

---
+++ Fergun/Modules/Commands.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.OCR;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Fergun.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+		[Command("help")]
+		public async Task Help()
+		{
+			await ReplyAsync("hmm...");
+		}
+
+		[RequireOwner]
+		[Command("game")]
+		[Summary("Sets the game status of the bot.")]
+		public async Task GameStatus([Remainder] [Summary("The text to set")] string text)
+		{
+			await Context.Client.SetGameAsync(text);
+			//IActivity Activity;
+			//Activity.Name = text;
+			//Activity.Type = ActivityType.Watching;
+			//await Context.Client.SetActivityAsync(Activity);
+		}
+
+		[RequireOwner]
+		[Command("status")]
+		[Summary("Sets the status of the bot.")]
+		public async Task Status([Summary("The status to set (0 - 5)")] int status)
+		{
+			if (status >= 0 && status <= 5)
+			{
+				await Context.Client.SetStatusAsync((UserStatus)status);
+				await Task.CompletedTask;
+			}
+		}
+
+		[RequireOwner]
+		[Command("say")]
+		[Summary("Echoes a message.")]
+		public async Task SayAsync([Remainder] [Summary("The text to echo")] string text)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			await ReplyAsync(text);
+		}
+
+		[Command("vapor")]
+		[Summary("Converts a text to vaporwave.")]
+		public async Task Vapor([Remainder] [Summary("The text to convert")] string text)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			await ReplyAsync(Fullwidth(text));
+		}
+
+		[Command("randomize"), Alias("shuffle")]
+		[Summary("Randomizes a text.")]
+		public async Task Randomize([Remainder] [Summary("The text to randomize")] string text)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			Random r = new Random();
+			await ReplyAsync(new string(text.ToCharArray().OrderBy(s => (r.Next(2) % 2) == 0).ToArray()));
+		}
+
+		[Command("repeat")]
+		[Summary("Repeats a text a number of times.")]
+		public async Task RepeatString([Summary("The text to randomize")] string text, int times) //TODO: Remainder
+		{
+			if (times > 0 && text.Length * times <= 2000)
+			{
+				await Context.Channel.TriggerTypingAsync();
+				await ReplyAsync(Repeat(text, times));
+			}
+		}
+
+		[Command("reverse")]
+		[Summary("Reverses a text.")]
+		public async Task ReverseString([Remainder] [Summary("The text to reverse")] string text)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			await ReplyAsync(Reverse(text));
+		}
+
+		[Command("ping")]
+		public async Task Ping()
+		{
+			await ReplyAsync("Pong");
+		}
+
+		[Command("square")]
+		[Summary("Squares a number.")]
+		public async Task SquareAsync([Summary("The number to square.")] int num)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			await ReplyAsync($"{num}\u00B2 = {Math.Pow(num, 2)}");
+		}
+
+		[Command("fire")]
+		[Summary("Adds a fire reaction")]
+		public async Task Fire(SocketUserMessage Message = null)
+		{
+			var temp = Message ?? Context.Message;
+			var emoji = new Emoji("\uD83D\uDD25");
+			await temp.AddReactionAsync(emoji);
+		}
+
+		[Command("userinfo")]
+		[Summary("Returns info about the current user, or the user parameter, if one passed.")]
+		[Alias("user", "whois")]
+		public async Task UserInfoAsync([Summary("The (optional) user to get info from")] SocketUser user = null)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			var userInfo = user ?? Context.Client.CurrentUser;
+			await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+		}
+
+		[RequireUserPermission(GuildPermission.KickMembers , Group = "Permission")]
+		[Command("kick")]
+		[Summary("Kicks an user")]
+		public async Task Kick(IGuildUser user = null, string reason = "")
+		{
+			await Context.Channel.TriggerTypingAsync();
+			if (user == null)
+			{
+				await ReplyAsync("idk who to kick??¿¿");
+			}
+			else
+			{
+				try
+				{
+					await user.KickAsync(reason);
+					await ReplyAsync($"Kicked {user.Mention}");
+				}
+				catch (Exception e)
+				{
+					await ReplyAsync(e.Message);
+				}
+			}
+		}
+
+		[RequireUserPermission(GuildPermission.BanMembers, Group = "Permission")]
+		[Command("ban")]
+		[Summary("Bans an user")]
+		public async Task Ban(IGuildUser user = null, string reason = "")
+		{
+			await Context.Channel.TriggerTypingAsync();
+			if (user == null)
+			{
+				await ReplyAsync("idk who to ban??¿¿");
+			}
+			else if (Context.Guild.GetBanAsync(user) != null)
+			{
+				await ReplyAsync("User is already banned.");
+			}
+			else
+			{
+				try
+				{
+					await user.BanAsync(0, reason);
+					await ReplyAsync($"Banned {user.Mention}");
+				}
+				catch (Exception e)
+				{
+					await ReplyAsync(e.Message);
+				}
+			}
+		}
+
+		[RequireUserPermission(GuildPermission.BanMembers, Group = "Permission")]
+		[Command("hackban")]
+		[Summary("Hackbans an user")]
+		public async Task Hackban(ulong userID = 0, string reason = "")
+		{
+			await Context.Channel.TriggerTypingAsync();
+			if (userID == 0)
+			{
+				await ReplyAsync("idk who to hackban??¿¿");
+			}
+			else
+			{
+				try
+				{
+					await Context.Guild.AddBanAsync(userID, 0, reason);
+					//await ReplyAsync($"Hackbanned {Context.Client.GetUser(userID).Mention} ({userID})");
+					await ReplyAsync($"Hackbanned {userID}");
+				}
+				catch (Exception e)
+				{
+					await ReplyAsync(e.Message);
+				}
+			}
+		}
+
+		[RequireUserPermission(GuildPermission.KickMembers, Group = "Permission")]
+		[Command("unban")]
+		public async Task Unban(IGuildUser user)
+		{
+			await Context.Channel.TriggerTypingAsync();
+			try
+			{
+				await Context.Guild.RemoveBanAsync(user);
+				await ReplyAsync($"Unbanned {user.Mention}");
+			}
+			catch (Exception e)
+			{
+				await ReplyAsync(e.Message); //"Could not unban the user."
+			}
+		}
+
+		[Command("invite")]
+		public async Task Invite()
+		{
+			await Context.Channel.TriggerTypingAsync();
+			await ReplyAsync("<https://discordapp.com/api/oauth2/authorize?client_id=680507783359365121&permissions=8&scope=bot>");
+		}
+
+		[Command("ocr")]
+		public async Task OCR(string url = "")
+		{
+			if (url == "")
+				url = Context.Message.Attachments.ElementAt(0).Url;
+			await Context.Channel.TriggerTypingAsync();
+			//byte[] bytes;
+			//WebClient wc = new WebClient();
+			try
+			{
+				//bytes = wc.DownloadData(attachments.ElementAt(0).Url);
+				//MemoryStream ms = new MemoryStream(bytes);
+				//System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+				// initialize an instance of OcrEngine
+				OcrEngine ocrEngine = new OcrEngine();
+				// set the Image property by loading the image from remote location
+				ocrEngine.Image = ImageStream.FromUrl(url);
+				// run recognition process
+				ocrEngine.Process();
+				// display the recognized text
+				await ReplyAsync(ocrEngine.Text.ToString());
+			}
+			catch (Exception e)
+			{
+				await ReplyAsync(e.Message + "\n" + e.StackTrace);
+			}
+		}
+
+		/// <summary>
+		/// Converts a string to it's full width form.
+		/// </summary>
+		/// <param name="Input">A string to convert.</param>
+		private static string Fullwidth(string Input)
+		{
+			string Output = "";
+			foreach (char currentchar in Input.ToCharArray())
+			{
+				if (0x21 <= currentchar && currentchar <= 0x7E) // ASCII chars, excluding space
+					Output += (char)(currentchar + 0xFEE0);
+				else if (currentchar == 0x20)
+					Output += (char)0x3000;
+				else
+					Output += currentchar;
+			}
+			return Output;
+		}
+
+		private static string Repeat(string Input, int Count)
+		{
+			return string.Concat(Enumerable.Repeat(Input, Count));
+		}
+
+		private static string Reverse(string Input)
+		{
+			return new string(Input.Reverse().ToArray());
+		}
+
+		private async Task TryReplyAsync(string text)
+		{
+			try
+			{
+				await ReplyAsync(text);
+			}
+			catch (Exception e)
+			{
+				await ReplyAsync(e.Message);
+			}
+		}
+	}
+}
---
+++ Fergun/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Fergun
+{
+    class Program
+    {
+		public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+		private DiscordSocketConfig _config;
+        private DiscordSocketClient _client;
+		private CommandService _commands;
+		private IServiceProvider _services;
+
+		public static ulong BotOwnerID = 385963075892936706;
+
+		public async Task MainAsync()
+        {
+			// When working with events that have Cacheable<IMessage, ulong> parameters,
+			// you must enable the message cache in your config settings if you plan to
+			// use the cached message entity. 
+			_config = new DiscordSocketConfig { MessageCacheSize = 100 };
+			_client = new DiscordSocketClient(_config);
+			_commands = new CommandService();
+			_services = new ServiceCollection()
+				.AddSingleton(_client)
+				.AddSingleton(_commands)
+				.BuildServiceProvider();
+				
+			// Hardcoded token 🤔
+            string token = "Token";
+
+			_client.Log += Log;
+
+			CommandHandler CHandler = new CommandHandler(_client, _commands);
+			await CHandler.InstallCommandsAsync();
+
+			await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+			await _client.SetGameAsync("-", null, ActivityType.Streaming);
+			//await _client.SetActivityAsync();
+			await _client.SetStatusAsync(UserStatus.Online);
+
+			_client.MessageUpdated += MessageUpdated;
+			_client.Ready += () =>
+			{
+				Console.WriteLine("Bot is connected!");
+				return Task.CompletedTask;
+			};
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+        }
+
+		private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+		{
+			// If the message was not in the cache, downloading it will result in getting a copy of `after`.
+			var message = await before.GetOrDownloadAsync();
+			Console.WriteLine($"{message} -> {after}");
+			
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+        public class CommandHandler
+        {
+            private readonly DiscordSocketClient _client;
+            private readonly CommandService _commands;
+
+            // Retrieve client and CommandService instance via ctor
+            public CommandHandler(DiscordSocketClient client, CommandService commands)
+            {
+                _commands = commands;
+                _client = client;
+            }
+
+            public async Task InstallCommandsAsync()
+            {
+                // Hook the MessageReceived event into our command handler
+                _client.MessageReceived += HandleCommandAsync;
+
+                // Here we discover all of the command modules in the entry 
+                // assembly and load them. Starting from Discord.NET 2.0, a
+                // service provider is required to be passed into the
+                // module registration method to inject the 
+                // required dependencies.
+                //
+                // If you do not use Dependency Injection, pass null.
+                // See Dependency Injection guide for more information.
+                await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            }
+
+            private async Task HandleCommandAsync(SocketMessage messageParam)
+            {
+                // Don't process the command if it was a system message
+                var message = messageParam as SocketUserMessage;
+                if (message == null) return;
+
+                // Create a number to track where the prefix ends and the command begins
+                int argPos = 0;
+
+                // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+                if (!(message.HasCharPrefix('-', ref argPos) ||
+                    message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                    message.Author.IsBot)
+                    return;
+
+                // Create a WebSocket-based command context based on the message
+                var context = new SocketCommandContext(_client, message);
+
+                // Execute the command with the command context we just
+                // created, along with the service provider for precondition checks.
+                await _commands.ExecuteAsync(
+                    context: context,
+                    argPos: argPos,
+                    services: null);
+            }
+        }
+
+		/*
+		public class LoggingService
+		{
+			public LoggingService(DiscordSocketClient client, CommandService command)
+			{
+				client.Log += LogAsync;
+				command.Log += LogAsync;
+			}
+			private Task LogAsync(LogMessage message)
+			{
+				if (message.Exception is CommandException cmdException)
+				{
+					Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+						+ $" failed to execute in {cmdException.Context.Channel}.");
+					Console.WriteLine(cmdException);
+				}
+				else
+					Console.WriteLine($"[General/{message.Severity}] {message}");
+
+				return Task.CompletedTask;
+			}
+		}
+		*/
+	}
+}
